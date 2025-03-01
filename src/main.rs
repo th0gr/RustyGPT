ff8980e22225e2ffb51ecd4f63debb7f39990a89
Thomas Graham <thomashgraham@gmail.com>
2025-03-01 14:17:46 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,449 @@
+use pyo3::prelude::*;
+use pyo3::types::{PyDict, PyList, PyModule};
+use itertools::multizip;
+use pyo3_ffi::c_str;
+use ndarray::{Array2, Axis};
+use numpy::{PyArrayDyn, PyArrayMethods};
+use std::collections::HashMap;
+use std::f32::consts::E;
+
+#[derive(Debug)]
+struct Params {
+    encoder: Py<PyAny>,
+    hparams: HashMap<String, Value>,
+    params: HashMap<String, Value>,
+    input_ids: Vec<i64>,
+}
+
+#[derive(Debug)]
+enum Value {
+    String(String),
+    Bool(bool),
+    Int(i64),
+    Float(f32),
+    Hashmap(HashMap<String, Value>),
+    List(Vec<Value>),
+    NumpyArray(Array2<f32>),
+}
+
+fn extract_hashmap(py_any: &pyo3::Bound<'_, PyAny>) -> PyResult<HashMap<String, Value>> {
+    let dict = py_any.downcast::<PyDict>()?;
+    // py_any.extract::<HashMap<String, Value>>()
+
+    let mut result = HashMap::new();
+
+    for (key, value) in dict.iter() {
+        let key_str = key.extract::<String>()?;
+        let val = if value.is_instance_of::<PyDict>() {
+            Value::Hashmap(extract_hashmap(&value)?)
+        } else if value.is_instance_of::<PyList>() {
+            let list = value.downcast::<PyList>()?;
+            let mut vec = Vec::new();
+            for item in list.iter() {
+                if let Ok(dict) = item.downcast::<PyDict>() {
+                    vec.push(Value::Hashmap(extract_hashmap(&dict)?));
+                } else if let Ok(f) = item.extract::<f32>() {
+                    vec.push(Value::Float(f));
+                } else if let Ok(i) = item.extract::<i64>() {
+                    vec.push(Value::Int(i));
+                } else if let Ok(s) = item.extract::<String>() {
+                    vec.push(Value::String(s));
+                } else if let Ok(b) = item.extract::<bool>() {
+                    vec.push(Value::Bool(b));
+                }
+            }
+            Value::List(vec)
+        } else if let Ok(py_arr) = value.extract::<Py<PyArrayDyn<f32>>>() {
+            let mut array = Python::with_gil(|py| {
+                let array_ref = py_arr.bind(py);
+                // println!("array: {:?}", array_ref);
+                let mut res = array_ref.readonly().as_array().to_owned().into_dyn();
+                if res.ndim() != 2 {
+                    res = res.insert_axis(Axis(1));
+                }
+                //.into_dyn().into_dimensionality::<ndarray::Dim<[usize; 2]>>();
+                res.into_dimensionality::<ndarray::Dim<[usize; 2]>>()
+                    .unwrap()
+            });
+            // if array.ndim() != 2 {
+            //     array = array.insert_axis(Axis(1));
+            // }
+            // println!("array: {:?}", array);
+            Value::NumpyArray(array)
+        } else if let Ok(i) = value.extract::<i64>() {
+            Value::Int(i)
+        } else if let Ok(f) = value.extract::<f32>() {
+            Value::Float(f)
+        } else if let Ok(s) = value.extract::<String>() {
+            Value::String(s)
+        } else if let Ok(b) = value.extract::<bool>() {
+            Value::Bool(b)
+        } else {
+            continue;
+        };
+        result.insert(key_str, val);
+    }
+    Ok(result)
+}
+
+// x is n_seq x n_embd (i.e. n_seq x 768 for gpt2)
+fn layer_norm(x: Array2<f32>, g: &Value, b: &Value, eps: f32) -> Result<Array2<f32>, String> {
+    let g = match g {
+        Value::NumpyArray(arr) => arr,
+        other => Err(format!("Expected Hashmap for g, got {:?}", other))?,
+    };
+    let b = match b {
+        Value::NumpyArray(arr) => arr,
+        other => Err(format!("Expected Hashmap for b, got {:?}", other))?,
+    };
+    // println!("x is {:?}", x);
+    // println!("g is {:?}", g);
+    // println!("b is {:?}", b);
+    // mean is n_seq. Have to insert axis to add back second dimension to make it n_seq x 1
+    // which allows proper broadcasting when we substract mean and divide by var + eps.
+    // each row (i.e. each token) gets normalized independently
+    let mean = x.mean_axis(Axis(1)).unwrap().insert_axis(Axis(1));
+    // println!("Mean is {:?}", mean);
+    let var = x.var_axis(Axis(1), 0.).insert_axis(Axis(1)); // n_seq x 1
+    // println!("Var is {:?}", var);
+    let x_norm = (x - mean) / (var + eps).sqrt();
+    // println!("x_norm is {:?}", x_norm);
+    // println!("g[0] is {:?}", g.clone().flatten());
+    // println!(
+    //     "g * x_norm is {:?}",
+    //     g.clone().flatten().to_owned() * x_norm.clone()
+    // );
+    // println!(
+    //     "g * x_norm + b is {:?}",
+    //     g.clone().flatten().to_owned() * x_norm.clone() + b.flatten()
+    // );
+    Ok(g.flatten().to_owned() * x_norm + b.flatten())
+}
+
+fn linear(x: Array2<f32>, w: &Value, b: &Value) -> Result<Array2<f32>, String> {
+    let w = match w {
+        Value::NumpyArray(arr) => arr,
+        other => Err(format!("Expected NumpyArray for w, got {:?}", other))?,
+    };
+    let b = match b {
+        Value::NumpyArray(arr) => arr,
+        other => Err(format!("Expected NumpyArray for b, got {:?}", other))?,
+    };
+    // println!("x is {:?}", x);
+    // println!("w is {:?}", w);
+    // println!("b is {:?}", b);
+    Ok(x.dot(w) + b.flatten())
+}
+
+pub fn softmax(mut array: Array2<f32>) -> Array2<f32> {
+    for mut row in array.axis_iter_mut(Axis(0)) {
+        for value in &mut row {
+            *value = E.powf(*value);
+        }
+
+        let sum: f32 = row.iter().sum();
+
+        for value in row {
+            *value /= sum;
+        }
+    }
+    array
+}
+
+fn gelu(x: Array2<f32>) -> Array2<f32> {
+    let sqrt_2_pi = (2.0f32 / std::f32::consts::PI).sqrt();
+    // use mapv to apply the function to each element of the array
+    x.mapv(|x| {
+        0.5 * x * (1.0 + (sqrt_2_pi * (x + 0.044715 * x.powi(3))).tanh())
+    })
+}
+
+fn ffn(x: Array2<f32>, c_fc: &Value, c_proj: &Value) -> Result<Array2<f32>, String> {
+    let c_fc = match c_fc {
+        Value::Hashmap(hash) => hash,
+        other => Err(format!("Expected Hashmap for c_fc, got {:?}", other))?,
+    };
+    let c_proj = match c_proj {
+        Value::Hashmap(arr) => arr,
+        other => Err(format!("Expected Hashmap for f_proj, got {:?}", other))?,
+    };
+
+    let ffn1 = gelu(linear(x.clone(), &c_fc["w"], &c_fc["b"])?);
+    let ffn2 = linear(ffn1, &c_proj["w"], &c_proj["b"])?;
+    Ok(ffn2)
+}
+
+fn attention(q: &Array2<f32>, k: &Array2<f32>, v: &Array2<f32>, mask: &Array2<f32>) -> Array2<f32> {
+    // let (int, _) = (q.dot(&k.t()) / ((q.dim().1) as f32).sqrt() + mask).into_raw_vec_and_offset();
+    // let first_calc = q.dot(&k.t());
+    // println!("first_calc is {:?}", first_calc);
+    // let square_root = (q.dim().1 as f32).sqrt();
+    // println!("square_root is {:?}", square_root);
+    // let second_calc = first_calc.clone() / square_root;
+    // println!("second_calc is {:?}", second_calc);
+    // let third_calc = second_calc + mask;
+    // println!("third_calc is {:?}", third_calc);
+    // let int = (q.dot(&k.t())) / (q.dim().1 as f32).sqrt() + mask;
+    // println!("int is {:?}", int);
+    // let res = softmax(int);
+    // println!("res is {:?}", res);
+    // let fin = res.dot(v);
+    // println!("final is {:?}", fin);
+    softmax((q.dot(&k.t())) / (q.dim().1 as f32).sqrt() + mask).dot(v)
+    // softmax(q.dot(&k.t()) / ((q.dim().1) as f32).sqrt() + mask).dot(v);
+}
+
+// c_attn contains the weights and biases for q, k and v in th the attention module.
+fn mha(x: Array2<f32>, c_attn: &Value, c_proj: &Value, n_head: i64) -> Result<Array2<f32>, String> {
+    // println!("In MHA");
+    let c_attn = match c_attn {
+        Value::Hashmap(hash) => hash,
+        other => Err(format!("Expected Hashmap for c_attn, got {:?}", other))?,
+    };
+    let c_proj = match c_proj {
+        Value::Hashmap(hash) => hash,
+        other => Err(format!("Expected Hashmap for c_proj, got {:?}", other))?,
+    };
+    // println!("x before linear operation is {:?}", x);
+    let x = linear(x, &c_attn["w"], &c_attn["b"]).unwrap();
+    // println!("x after linear operation is {:?}", x);
+    // qkv_heads = list(map(lambda x: np.split(x, n_head, axis=-1), np.split(x, 3, axis=-1)))
+    // First split x into 3 parts along the last axis (q, k, v)
+    // println!("x shape is {:?}", x.shape());
+    let qkv = x.axis_chunks_iter(Axis(1), x.shape()[1] / 3);
+    // println!("qkv is {:?}", qkv);
+    // Then split each of those parts into n_head pieces
+    let qkv_heads: Vec<Vec<Array2<f32>>> = qkv
+        .map(|chunk| {
+            chunk
+                .axis_chunks_iter(Axis(1), chunk.shape()[1] / n_head as usize)
+                .map(|view| view.to_owned())
+                .collect()
+        })
+        .collect();
+
+    // println!("qkv_heads is {:?}", qkv_heads);
+
+    // Create causal mask to hide future tokens from being attended to.
+    // Causal mask is a square matrix of shape (seq_len, seq_len) where the left diagonal side is 1 and the rest is -1e10.
+    // TODO: should this be 1
+    let seq_len = x.shape()[0];
+    let mut causal_mask = Array2::<f32>::zeros((seq_len, seq_len));
+    for i in 0..seq_len {
+        for j in (i + 1)..seq_len {
+            causal_mask[[i, j]] = -1e10;
+        }
+    }
+    // println!("Causal Mask is {:?}", causal_mask);
+
+    let mut out_heads: Vec<Array2<f32>> = Vec::new();
+    for (q, k, v) in multizip((qkv_heads[0].iter(), qkv_heads[1].iter(), qkv_heads[2].iter())) {
+        // println!("Q is {:?}", q);
+        // println!("K is {:?}", k);
+        // println!("V is {:?}", v);
+        out_heads.push(attention(q, k, v, &causal_mask));
+    }
+    // println!("out_heads is {:?}", out_heads);
+    // merge heads
+    let merged_out_heads = ndarray::concatenate(Axis(1), &out_heads.iter().map(|x| x.view()).collect::<Vec<_>>()).unwrap();
+    // println!("Concatenated is {:?}", merged_out_heads);
+    // out projection
+    let x = linear(merged_out_heads, &c_proj["w"], &c_proj["b"]).unwrap();
+    // println!("output of mha is {:?}", x);
+    Ok(x)
+}
+
+fn transformer_block(
+    x: Array2<f32>,
+    mlp: &HashMap<String, Value>,
+    attn: &HashMap<String, Value>,
+    ln_1: &HashMap<String, Value>,
+    ln_2: &HashMap<String, Value>,
+    n_head: i64,
+) -> Array2<f32> {
+    // println!("In Transformer Block...");
+    // TODO: is there a way to remove the clone?
+    let normalized_x = layer_norm(x.clone(), &ln_1["g"], &ln_1["b"], 1e-5).unwrap();
+    // println!("Layer Norm is {:?}", normalized_x);
+    let mha_out = x + mha(normalized_x, &attn["c_attn"], &attn["c_proj"], n_head).unwrap();
+    // println!("MHA_Out is {:?}", mha_out);
+    let normalized_mha_out = layer_norm(mha_out.clone(), &ln_2["g"], &ln_2["b"], 1e-5).unwrap();
+    let ffn_out = ffn(normalized_mha_out, &mlp["c_fc"], &mlp["c_proj"]).unwrap();
+    // println!("FFN_Out is {:?}", ffn_out);
+    mha_out + ffn_out
+}
+
+fn gpt2(
+    inputs: &Vec<i64>,
+    wte: &Array2<f32>,
+    wpe: &Array2<f32>,
+    blocks: &Vec<Value>,
+    ln_f: &HashMap<String, Value>,
+    n_head: i64,
+) -> Result<Array2<f32>, String> {
+    // println!("In GPT2");
+    // println!("inputs: {:?}", inputs);
+    // println!("wte: {:?}", wte);
+    // println!("wpe: {:?}", wpe);
+    // println!("blocks: {:?}", blocks);
+    // println!("ln_f: {:?}", ln_f);
+    // println!("n_head: {:?}", n_head);
+
+    // lookup the embeddings for the input tokens in the wte (word token embedding) matrix
+    let indices: Vec<usize> = inputs.iter().map(|&x| x as usize).collect();
+    let token_embed = wte.select(Axis(0), &indices);
+    // lookup the positional encoding for the input tokens in the wpe (word position embedding) matrix
+    let indices: Vec<usize> = (0..inputs.len()).collect();
+    let position_emb = wpe.select(Axis(0), &indices);
+    // combine the token embeddings and positional embeddings
+    let mut x = token_embed + position_emb;
+    // println!("x: {:?}", x);
+    for block in blocks {
+        let this_block = match block {
+            Value::Hashmap(map) => map,
+            other => Err(format!(
+                "Expected Hashmap for transformer block, got {:?}",
+                other
+            ))?,
+        };
+        let attn = match &this_block["attn"] {
+            Value::Hashmap(map) => map,
+            other => Err(format!("Expected Hashmap for attn, got {:?}", other))?,
+        };
+        let mlp = match &this_block["mlp"] {
+            Value::Hashmap(map) => map,
+            other => Err(format!("Expected Hashmap for mlp, got {:?}", other))?,
+        };
+        let ln_1 = match &this_block["ln_1"] {
+            Value::Hashmap(map) => map,
+            other => Err(format!("Expected Hashmap for ln_1, got {:?}", other))?,
+        };
+        let ln_2 = match &this_block["ln_2"] {
+            Value::Hashmap(map) => map,
+            other => Err(format!("Expected Hashmap for ln_2, got {:?}", other))?,
+        };
+        x = transformer_block(x, mlp, attn, ln_1, ln_2, n_head);
+        // println!("x after transformer_block: {:?}", x);
+    }
+    Ok(layer_norm(x, &ln_f["g"], &ln_f["b"], 1e-5).unwrap().dot(&wte.t()))
+}
+
+fn generate(
+    mut inputs: Vec<i64>,
+    params: HashMap<String, Value>,
+    n_head: i64,
+    n_tokens_to_generate: usize,
+) -> Vec<i64> {
+    // println!("In Generate...");
+    // println!("blocks: {:?}", params["blocks"]);
+    let wte = match &params["wte"] {
+        Value::NumpyArray(arr) => arr,
+        _ => panic!("Expected NumpyArray"),
+    };
+
+    let wpe = match &params["wpe"] {
+        Value::NumpyArray(arr) => arr,
+        _ => panic!("Expected NumpyArray"),
+    };
+
+    let blocks = match &params["blocks"] {
+        Value::List(list) => list,
+        _ => panic!("Expected List for Blocks"),
+    };
+    let ln_f = match &params["ln_f"] {
+        Value::Hashmap(map) => map,
+        _ => {
+            // println!("ln_f: {:?}", params["ln_f"]);
+            panic!("Expected List for ln_f");
+        }
+    };
+
+    for _ in 0..n_tokens_to_generate {
+        let logits = gpt2(&inputs, wte, wpe, blocks, ln_f, n_head).unwrap();
+        // get the last row (i.e. the last token)
+        let last_row = logits.row(logits.nrows() - 1);
+        // argmax
+        let next_id = last_row.iter()
+            .enumerate()
+            .max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
+            .map(|(index, _)| index)
+            .unwrap();
+        // append prediction to input
+        inputs.push(next_id as i64);
+    }
+    inputs[inputs.len() - n_tokens_to_generate..].to_vec()
+}
+
+fn main() -> PyResult<()> {
+    pyo3::prepare_freethreaded_python();
+    // println!("Starting up...");
+    let prompt = "Alan Turing theorized that computers would one day become";
+    let n_tokens_to_generate = 5;
+
+    // load encoder, hparams, and params from the released open-ai gpt-2 files
+    let py_foo = c_str!(include_str!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/utils.py"
+    )));
+    let from_python: Py<PyAny> = Python::with_gil(|py| -> PyResult<Py<PyAny>> {
+        let module = PyModule::from_code(py, py_foo, c_str!("utils.py"), c_str!("utils.py"))?;
+        let func = module.getattr("load_encoder_hparams_and_params")?;
+        // println!("Calling Python function...");
+        let result = func.call1(("124M", "models"))?;
+        // println!("Result from Python: {:?}", result);
+        Ok(result.into())
+    })?;
+
+    // Extract the tuple elements
+    let model_params = Python::with_gil(|py| -> PyResult<Params> {
+        // Convert Py<PyAny> to a reference with lifetime.
+        let tuple = from_python.bind(py);
+        // Access tuple elements using indexing.
+        let encoder = tuple.get_item(0)?;
+        let hparams = tuple.get_item(1)?;
+        let params = tuple.get_item(2)?;
+
+        // println!("encoder: {:?}", encoder);
+        // println!("hparams: {:?}", hparams);
+        // println!("params: {:?}", params);
+
+        // encode the input string using the BPE tokenizer
+        let input_ids = encoder.call_method1("encode", (prompt,))?;
+        let input_ids_vec = input_ids.extract()?;
+        // println!("input ids: {:?}", input_ids);
+
+        let new_hparams = extract_hashmap(&hparams);
+        let new_params = extract_hashmap(&params);
+
+        let model_params = Params {
+            encoder: encoder.into(),
+            hparams: new_hparams.unwrap(),
+            params: new_params.unwrap(),
+            input_ids: input_ids_vec,
+        };
+
+        Ok(model_params)
+    })?;
+
+    // println!("Model params are {:?}", model_params);
+    // println!("hparams are {:?}", model_params.hparams);
+    // make sure we we are not suprassing the max sequence length of our model
+    let n_ctx = match model_params.hparams["n_ctx"] {
+        Value::Int(x) => x,
+        _ => panic!("n_ctx is not an integer"),
+    };
+    let input_ids = model_params.input_ids;
+    assert!(input_ids.len() + n_tokens_to_generate < n_ctx as usize);
+
+    let output_ids = generate(input_ids, model_params.params, 12, n_tokens_to_generate);
+    // let output_ids = vec![15496, 11, 995, 0];
+    Python::with_gil(|py| -> PyResult<_> {
+        let encoder = model_params.encoder.bind(py);
+        let output_ids = encoder.call_method1("decode", (output_ids,))?;
+        println!("output_ids: {:?}", output_ids);
+        Ok(())
+    })?;
+
+    // println!("finished");
+    Ok(())
+}
---
+++ utils.py
@@ -0,0 +1,204 @@
+import json
+import os
+import re
+
+import numpy as np
+import requests
+import tensorflow as tf
+from tqdm import tqdm
+
+# from encoder import get_encoder
+
+
+def download_gpt2_files(model_size, model_dir):
+    assert model_size in ["124M", "355M", "774M", "1558M"]
+    for filename in [
+        "checkpoint",
+        "encoder.json",
+        "hparams.json",
+        "model.ckpt.data-00000-of-00001",
+        "model.ckpt.index",
+        "model.ckpt.meta",
+        "vocab.bpe",
+    ]:
+        url = "https://openaipublic.blob.core.windows.net/gpt-2/models"
+        r = requests.get(f"{url}/{model_size}/{filename}", stream=True)
+        r.raise_for_status()
+
+        with open(os.path.join(model_dir, filename), "wb") as f:
+            file_size = int(r.headers["content-length"])
+            chunk_size = 1000
+            with tqdm(
+                ncols=100,
+                desc="Fetching " + filename,
+                total=file_size,
+                unit_scale=True,
+                unit="b",
+            ) as pbar:
+                # 1k for chunk_size, since Ethernet packet size is around 1500 bytes
+                for chunk in r.iter_content(chunk_size=chunk_size):
+                    f.write(chunk)
+                    pbar.update(chunk_size)
+
+
+def load_gpt2_params_from_tf_ckpt(tf_ckpt_path, hparams):
+    def set_in_nested_dict(d, keys, val):
+        if not keys:
+            return val
+        if keys[0] not in d:
+            d[keys[0]] = {}
+        d[keys[0]] = set_in_nested_dict(d[keys[0]], keys[1:], val)
+        return d
+
+    params = {"blocks": [{} for _ in range(hparams["n_layer"])]}
+    for name, _ in tf.train.list_variables(tf_ckpt_path):
+        array = np.squeeze(tf.train.load_variable(tf_ckpt_path, name))
+        name = name[len("model/") :]
+        if name.startswith("h"):
+            m = re.match(r"h([0-9]+)/(.*)", name)
+            n = int(m[1])
+            sub_name = m[2]
+            set_in_nested_dict(params["blocks"][n], sub_name.split("/"), array)
+        else:
+            set_in_nested_dict(params, name.split("/"), array)
+
+    return params
+
+
+def load_encoder_hparams_and_params(model_size, models_dir):
+    assert model_size in ["124M", "355M", "774M", "1558M"]
+
+    model_dir = os.path.join(models_dir, model_size)
+    tf_ckpt_path = tf.train.latest_checkpoint(model_dir)
+    if not tf_ckpt_path:  # download files if necessary
+        os.makedirs(model_dir, exist_ok=True)
+        download_gpt2_files(model_size, model_dir)
+        tf_ckpt_path = tf.train.latest_checkpoint(model_dir)
+
+    encoder = get_encoder(model_size, models_dir)
+    hparams = json.load(open(os.path.join(model_dir, "hparams.json")))
+    params = load_gpt2_params_from_tf_ckpt(tf_ckpt_path, hparams)
+
+    return encoder, hparams, params
+
+
+
+
+"""Byte pair encoding utilities.
+
+Copied from: https://github.com/openai/gpt-2/blob/master/src/encoder.py.
+"""
+from functools import lru_cache
+
+import regex as re
+
+
+@lru_cache()
+def bytes_to_unicode():
+    """
+    Returns list of utf-8 byte and a corresponding list of unicode strings.
+    The reversible bpe codes work on unicode strings.
+    This means you need a large # of unicode characters in your vocab if you want to avoid UNKs.
+    When you're at something like a 10B token dataset you end up needing around 5K for decent coverage.
+    This is a significant percentage of your normal, say, 32K bpe vocab.
+    To avoid that, we want lookup tables between utf-8 bytes and unicode strings.
+    And avoids mapping to whitespace/control characters the bpe code barfs on.
+    """
+    bs = list(range(ord("!"), ord("~") + 1)) + list(range(ord("¡"), ord("¬") + 1)) + list(range(ord("®"), ord("ÿ") + 1))
+    cs = bs[:]
+    n = 0
+    for b in range(2**8):
+        if b not in bs:
+            bs.append(b)
+            cs.append(2**8 + n)
+            n += 1
+    cs = [chr(n) for n in cs]
+    return dict(zip(bs, cs))
+
+
+def get_pairs(word):
+    """Return set of symbol pairs in a word.
+    Word is represented as tuple of symbols (symbols being variable-length strings).
+    """
+    pairs = set()
+    prev_char = word[0]
+    for char in word[1:]:
+        pairs.add((prev_char, char))
+        prev_char = char
+    return pairs
+
+
+class Encoder:
+    def __init__(self, encoder, bpe_merges, errors="replace"):
+        self.encoder = encoder
+        self.decoder = {v: k for k, v in self.encoder.items()}
+        self.errors = errors  # how to handle errors in decoding
+        self.byte_encoder = bytes_to_unicode()
+        self.byte_decoder = {v: k for k, v in self.byte_encoder.items()}
+        self.bpe_ranks = dict(zip(bpe_merges, range(len(bpe_merges))))
+        self.cache = {}
+
+        # Should have added re.IGNORECASE so BPE merges can happen for capitalized versions of contractions
+        self.pat = re.compile(r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")
+
+    def bpe(self, token):
+        if token in self.cache:
+            return self.cache[token]
+        word = tuple(token)
+        pairs = get_pairs(word)
+
+        if not pairs:
+            return token
+
+        while True:
+            bigram = min(pairs, key=lambda pair: self.bpe_ranks.get(pair, float("inf")))
+            if bigram not in self.bpe_ranks:
+                break
+            first, second = bigram
+            new_word = []
+            i = 0
+            while i < len(word):
+                try:
+                    j = word.index(first, i)
+                    new_word.extend(word[i:j])
+                    i = j
+                except:
+                    new_word.extend(word[i:])
+                    break
+
+                if word[i] == first and i < len(word) - 1 and word[i + 1] == second:
+                    new_word.append(first + second)
+                    i += 2
+                else:
+                    new_word.append(word[i])
+                    i += 1
+            new_word = tuple(new_word)
+            word = new_word
+            if len(word) == 1:
+                break
+            else:
+                pairs = get_pairs(word)
+        word = " ".join(word)
+        self.cache[token] = word
+        return word
+
+    def encode(self, text):
+        bpe_tokens = []
+        for token in re.findall(self.pat, text):
+            token = "".join(self.byte_encoder[b] for b in token.encode("utf-8"))
+            bpe_tokens.extend(self.encoder[bpe_token] for bpe_token in self.bpe(token).split(" "))
+        return bpe_tokens
+
+    def decode(self, tokens):
+        text = "".join([self.decoder[token] for token in tokens])
+        text = bytearray([self.byte_decoder[c] for c in text]).decode("utf-8", errors=self.errors)
+        return text
+
+
+def get_encoder(model_name, models_dir):
+    with open(os.path.join(models_dir, model_name, "encoder.json"), "r") as f:
+        encoder = json.load(f)
+    with open(os.path.join(models_dir, model_name, "vocab.bpe"), "r", encoding="utf-8") as f:
+        bpe_data = f.read()
+    bpe_merges = [tuple(merge_str.split()) for merge_str in bpe_data.split("\n")[1:-1]]
+    return Encoder(encoder=encoder, bpe_merges=bpe_merges)
